5cb1ca25c3701b4cd4e37e3a87a71d32fcf2eb0b
3andne <3andne@github.com>
2021-07-13 10:53:28 +0000

    feat: uninit buffer

--- src/utils/copy_tcp.rs
+++ src/utils/copy_tcp.rs
@@ -1,13 +1,19 @@
+use std::mem::MaybeUninit;
 use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
 
 use anyhow::Result;
 
-const RELAY_BUFFER_SIZE: usize = 2048;
+const RELAY_BUFFER_SIZE: usize = 8192;
 pub async fn copy_tcp<R: AsyncRead + Unpin, W: AsyncWrite + Unpin>(
     r: &mut R,
     w: &mut W,
 ) -> Result<()> {
-    let mut buf = [0; RELAY_BUFFER_SIZE];
+    // safety: We don't realy care what's previouly in the buffer
+    let mut buf = unsafe {
+        let buf: [MaybeUninit<u8>; RELAY_BUFFER_SIZE] = MaybeUninit::uninit().assume_init();
+        std::mem::transmute::<_, [u8; RELAY_BUFFER_SIZE]>(buf)
+    };
+
     loop {
         let len = r.read(&mut buf).await?;
         if len == 0 {
