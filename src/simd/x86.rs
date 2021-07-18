7cefe3ec59a0d64d472f4ed062b7947993e45b72
3andne <3andne@github.com>
2021-07-18 12:57:55 +0800

    fix

--- src/simd/x86.rs
+++ src/simd/x86.rs
@@ -19,7 +19,7 @@ pub unsafe fn simd_trojan_password_compare(a: &[u8], b: &[u8]) -> bool {
     for i in 0..3 {
         let d0 = _mm_lddqu_si128(p0.offset(i * 16) as *const _);
         let d1 = _mm_lddqu_si128(p1.offset(i * 16) as *const _);
-        let eq = _mm_cmpeq_epi8(d0, d1);
+        let eq = _mm_movemask_epi8(_mm_cmpeq_epi8(d0, d1)) as u32;
         if eq != 0xffff {
             return false;
         }
@@ -27,6 +27,6 @@ pub unsafe fn simd_trojan_password_compare(a: &[u8], b: &[u8]) -> bool {
 
     let d0 = _mm_loadu_si64(p0.offset(48) as *const _);
     let d1 = _mm_loadu_si64(p1.offset(48) as *const _);
-    let eq = _mm_cmpeq_epi8(d0, d1);
+    let eq = _mm_movemask_epi8(_mm_cmpeq_epi8(d0, d1)) as u32;
     eq == 0xffff
 }
