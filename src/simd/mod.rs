3c0f22cfdd2e73996c1811ac37cde7189d6b02ae
3andne <3andne@github.com>
2021-07-19 10:11:14 +0800

    fix: attributes

--- src/simd/mod.rs
+++ src/simd/mod.rs
@@ -1,8 +1,8 @@
-pub mod neon;
+// pub mod neon;
 
-#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+#[cfg(all(feature = "nightly", any(target_arch = "x86", target_arch = "x86_64")))]
 pub mod x86;
-#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+#[cfg(all(feature = "nightly", any(target_arch = "x86", target_arch = "x86_64")))]
 pub use x86::trojan_password_compare;
 
 #[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
